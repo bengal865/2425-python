4817d2fece52aee741097946acb7a4505222a5f2
bengal865 <69059209+bengal865@users.noreply.github.com>
2024-05-30 13:08:14 -0400

    Update comment.py

--- variables-data-types/comment.py
+++ variables-data-types/comment.py
@@ -1,2 +1,10 @@
- # Say hello to everyone
+# Alyssa Robinson
+# 20 OCT 20XX
+# Python Print Function
+
+# Say hello to everyone
 print("Hello Python people!")
+
+# You can also comment out code (make it temporarily invisible to Python)
+print("Now you see me, Python!")
+# print("And now you don't!")
