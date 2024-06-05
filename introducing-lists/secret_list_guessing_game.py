201f86ce95769bdc0a660659d12637fc2cea7b4c
bengal865 <69059209+bengal865@users.noreply.github.com>
2024-06-05 08:04:46 -0400

    Update secret_list_guessing_game.py

--- introducing-lists/secret_list_guessing_game.py
+++ introducing-lists/secret_list_guessing_game.py
@@ -3,15 +3,26 @@
 secret_list = ["apple", "banana", "orange", "grape"]  
 
 # This function checks your guess and provides clues about the content of the list
+
+'''
+Start with broad guesses (e.g., "food") to narrow down the possibilities
+
+Use the length information to your advantage. For example, if the length of the list is 4 and you guessed a fruit that wasn't in the list, there still might be 3 other fruits!
+
+Refine your guess: Based on the clue, try to guess another item in the list. Keep guessing until you guess all the items 
+in the list
+'''
+
 def guess_check(guess):
   if guess in secret_list:
     print("Yes! " + guess + " is in the list.")
   else:
     print("Nope, " + guess + " is not in the list.")
-    # To help you guess, let's reveal the length of the list:", len(secret_list))
+    print(f"To help you guess, let's reveal the length of the list: {len(secret_list)}")
 
 # Start guessing! Use the guess_check function to get clues about what's contained in the list
 
 guess_check('hamburger')
-guess_check('potato chips')
+#guess_check('potato chips')
+
 
