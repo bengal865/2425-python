7defa5d315b4b588388f2b061fffc2f41c7fc175
bengal865 <69059209+bengal865@users.noreply.github.com>
2024-06-05 10:24:11 -0400

    Add files via upload

---
+++ introducing-lists/list-for-loop-demo.py
@@ -0,0 +1,28 @@
+# For loop with Python list demo
+
+# Create a list of superheroes
+superheroes = ["Iron Man", "Spider-Man", "Captain America", "Black Panther", "Hulk"]
+
+# Loop through the list using a FOR loop
+for superhero in superheroes:
+  # Print each superhero's name
+  print(superhero)
+print()
+
+# Create a list of ice cream flavors
+ice_cream_flavors = ["Vanilla", "Chocolate Chip Cookie Dough", "Mint Chocolate Chip", "Strawberry", "Coffee"]
+
+# Loop through the list using a FOR loop
+for ice_cream_flavor in ice_cream_flavors:
+  # Print each flavor
+  print(f"We have delicious {ice_cream_flavor} ice cream on sale today!")
+print()
+
+# Create a list of European capitals
+capitals = ["paris", "london", "berlin", "rome", "madrid"]
+
+# Loop through the list using a FOR loop
+for capital in capitals:
+  # Print each capital city
+  print(f'{capital.capitalize()} is a European capital I want to visit someday!')
+print()
