1575f701349b47d071840dfbd95b62d25add0e95
bengal865 <69059209+bengal865@users.noreply.github.com>
2024-06-06 05:07:24 -0400

    Added new Python file to repo

---
+++ variables-data-types/loop_quiz3.py
@@ -0,0 +1,26 @@
+num_quiz_scores = 6
+total = 0
+
+# Get first name from user for custom greeting
+first_name = input('Please enter your first name: (Example: Mike)\n').title()
+
+print(f'Hello, {first_name}! This script will calculate your average quiz score.')
+print()
+
+# Prompt user for the six quiz scores using a for loop
+quiz_scores = []
+for i in range(num_quiz_scores):
+  quiz_score = float(input(f'Enter a quiz score {i+1} of {num_quiz_scores}: (Example: 84)\n'))
+  quiz_scores.append(quiz_score)
+
+# Calculate sum and average
+total = sum(quiz_scores)
+average = total / num_quiz_scores
+
+# Display the individual quiz scores, then the average score
+print('Here are your individual quiz scores:')
+print(*quiz_scores)  # Use unpacking for cleaner output
+print()
+print(f'And your average is: {average}')
+print()
+print('Thank you, and goodbye!')
