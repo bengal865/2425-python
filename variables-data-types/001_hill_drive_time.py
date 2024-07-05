333893c38049f4ff200136deaeac4be60598ad03
bengal865 <69059209+bengal865@users.noreply.github.com>
2024-07-05 10:52:13 -0400

    Update 001_hill_drive_time.py

--- variables-data-types/001_hill_drive_time.py
+++ variables-data-types/001_hill_drive_time.py
@@ -1,17 +1,17 @@
-# Programmer: Nick H.
-# Date: 19.09.2018
-# Project: Drive
+# Student name
+# Current date
+# Calculating Travel Time
 
 # Prompts user for distance and speed, and converts the input into integers
 distance = int(input("How many miles are you driving? "))
 mph = int(input("How fast are you travelling? (in MPH) "))
 
 # Calculates travel time
-travelTime = round(distance / mph, 1)
+travel_time = round(distance / mph, 1)
 
 # Displays distance, mph, and travel time
 print()
-print(f"Distance: {distance} miles")
-print(f"Miles per Hour: {mph} mph")
+print(f"Distance: {distance} (miles)")
+print(f"Miles per hour: {mph} mph")
 print()
-print(f"Travel time: {travelTime} hours") 
+print(f"Travel time: {travel_time} (hours)") 
