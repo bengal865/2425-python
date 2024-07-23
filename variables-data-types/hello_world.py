0adf12cb5a7113fde1f89ed40eca1c5169ce8873
bengal865 <69059209+bengal865@users.noreply.github.com>
2024-07-23 10:32:03 -0400

    Added more code to script

--- variables-data-types/hello_world.py
+++ variables-data-types/hello_world.py
@@ -1,5 +1,16 @@
-message = "Hello Python world!"
-print(message)
+import time
 
-message = "Hello Python Crash Course world!"
-print(message)
+delay = 5
+
+message1 = "Hello world!"
+time.sleep(3)
+print(message1)
+
+message2 = "Welcome to Career Tech!"
+thanks = "Thank you for using my script today!"
+time.sleep(delay)
+print(message2)
+time.sleep(delay)
+print(thanks)
+time.sleep(delay)
+print("Goodbye!")
